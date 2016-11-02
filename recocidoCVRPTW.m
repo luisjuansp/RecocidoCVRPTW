249d98c5f4d44b74579c4e625d3be7b5d01938fd
Abraham Rodriguez <abraham.rq03@gmail.com>
2016-11-01 20:02:38 -0600

    dir

--- recocidoCVRPTW.m
+++ recocidoCVRPTW.m
@@ -1,8 +1,10 @@
 %% Problema de CVRPTW
 
 dir = '/home/lsanchez/Documents/Inteligencia Computacional/Software/Recocido/PSolomon';
+dir_abraham = 'C:/Users/Abraham/Develop/RecocidoCVRPTW/PSolomon';
+
 fname = 'rc106'; % archivo de datos
-[nc, x, y, d, e, l, s, numeroVehiculos, capacidad] = getDataCVRPTW(dir, fname);
+[nc, x, y, d, e, l, s, numeroVehiculos, capacidad] = getDataCVRPTW(dir_abraham, fname);
 
 costoVRP(nc, x, y, d, e, l, s, capacidad);
 
